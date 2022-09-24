17728383466ea869168ee0ae3e0c8ce26a7a20ae
Eduardo Alves Canuto <edutecni@gmail.com>
2022-09-24 17:28:08 -0300

    Context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Livraria.Data;
+using Livraria.Data.Models;
+
+namespace Livraria.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Genero> Generoes { get; set; }
+    }
+}
---
+++ Data/Models/Genero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livraria.Data.Models
+{
+    [Table("Genero")]
+    public class Genero
+    {
+        public Genero()
+        {
+
+        }
+
+        [Key]
+        [Required]
+        public int GeneroId { get; set; }
+        public string Descricao { get; set; }
+
+    }
+
+    
+}
